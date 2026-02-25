b46ae6ca900fd1c160938ce937209cbc5f17152e
mgnamdan <danny.ai.hub@gmail.com>
2026-02-25 17:24:24 -0500

    setup file architecture

--- calculator.py
+++ calculator.py
@@ -1,1 +1,46 @@
 # Hello from the file!
+
+# 1. Take in user input
+# 2. Decide what operation to perform based on the input
+# 3. Perform the operation
+# 4. Give the answer to the operation
+# 5. Ask if they want to perform another calculation
+# If yes:
+# 6. a. Repeat 1-5
+# 6. b. Shut off the application
+
+### PART ONE - Imports and Helper Functions ###
+def add(num1, num2):
+    pass
+
+
+def subtract(num1, num2):
+    pass
+
+
+def multiply(num1, num2):
+    pass
+
+
+def divide(num1, num2):
+    pass
+
+
+
+
+
+
+### PART TWO - main() Definition ###
+def main():
+    pass
+
+
+
+
+
+
+
+
+### PART THREE - main() Call ###
+if __name__ == "__main__":
+    main()
